33644c9d5b0febb1f21a6e29bd8d3a9c5bb02946
MeenaShankar <52780067+MeenaShankar@users.noreply.github.com>
2019-08-04 04:45:18 -0700

    Create list_dis_color.py

---
+++ list_dis_color.py
@@ -0,0 +1,5 @@
+color=['red','green','white','black']
+#len() used to find length of list
+n=len(color)
+print("First colour in the list is ",color[0])
+print("Last colour in the list is ",color[n-1])
