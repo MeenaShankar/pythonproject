87eb9733e3fa31bdee6e5f58c01fc8927a52bbb1
MeenaShankar <52780067+MeenaShankar@users.noreply.github.com>
2019-08-05 00:01:55 +0530

    Create sum19.py

---
+++ sum19.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter the 1st number:"))
+n2=int(input("Enter the 2nd number:"))
+sum=n1+n2
+if(sum>=15) and (sum<=20):
+ print("The sum value is less than 20 and greater than 15\nsum value is 20")
+else:
+ print("sum:",sum)
