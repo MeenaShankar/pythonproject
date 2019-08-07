69b22feba397aee1b2794abe23ebf32b25d36bde
MeenaShankar <52780067+MeenaShankar@users.noreply.github.com>
2019-08-07 03:12:30 -0700

    Create dis_details.py

---
+++ dis_details.py
@@ -0,0 +1,8 @@
+def details(name,age,address):
+    print("*-------------------------*")
+    print("Name:",name)
+    print("Age:",age)
+    print("Address:",address)
+    print("*-------------------------*")
+details("Meena",18,"chennai")
+
